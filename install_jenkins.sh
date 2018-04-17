e409c8cb1f6a41d572648a9f680e4a081cc09856
root <root@ip-172-31-19-255.us-west-2.compute.internal>
2018-04-17 19:12:11 +0000

    initial commit

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-mkdir -p /data
-cd /data
-wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.85/bin/apache-tomcat-7.0.85.tar.gz
-tar -zxvf apache-tomcat-7.0.85.tar.gz 
-mv apache-tomcat-7.0.85 jenkins
-cd jenkins/webapps
+
+java -version
+if [ $? = 0 ]
+then
+echo "ok to proceed.."
+else
+echo "Install Java & try again"
+fi
+
+
+
 
 if [ -f /usr/bin/wget ]
 then
@@ -14,13 +19,34 @@ echo "wget is not installed..installing it now.."
 yum install wget -y
 fi
 
-wget "https://updates.jenkins-ci.org/latest/jenkins.war"
-cd ../bin
-java -version
-if [ $? = 0 ]
-then
-./startup.sh
-else
-echo "Install Java & try again"
-fi
+setup_tomcat()
+{
+mkdir -p /data
+cd /data
+wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.85/bin/apache-tomcat-7.0.85.tar.gz
+tar -zxvf apache-tomcat-7.0.85.tar.gz
+mv /data/apache-tomcat-7.0.85 /data/jenkins
+sed -i.orig.bak 's/8080/8002/g' /data/jenkins/conf/server.xml
+}
 
+set_war()
+{
+cd /data/jenkins/webapps
+wget "https://updates.jenkins-ci.org/latest/jenkins.war"
+}
+
+start_jenkins()
+{
+cd /data/jenkins/bin
+./startup.sh
+sleep 10
+tail ../logs/catalina.out
+echo -e "\eJenkins Started successfully\e"
+}
+
+setup_jenkins()
+{
+setup_tomcat
+set_war
+start_jenkins
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+# myscripts
+
+1. execute "install_initial_sh"
+
+2. 
