fa52044c7acf99fef1605d1ff25b86ee97d63e8a
root <root@ip-172-31-19-255.us-west-2.compute.internal>
2018-04-21 11:25:20 +0000

    initial-commit

--- install_maven.sh
+++ install_maven.sh
@@ -3,3 +3,4 @@
 cd /data
      wget http://redrockdigimark.com/apachemirror/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
      tar -xzf apache-maven-3.5.3-bin.tar.gz
+mv apache-maven-3.5.3 maven
