cd10013d95f97ce40799716620a443375b70facd
root <root@ip-172-31-19-255.us-west-2.compute.internal>
2018-04-21 05:15:00 +0000

    initial-commit

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -6,5 +6,7 @@ tar -zxvf apache-tomcat-7.0.85.tar.gz
 mv apache-tomcat-7.0.85 apache-tomcat
 cd apache-tomcat/bin
 sed -i.orig.bak 's/8080/8001/g' /data/apache-tomcat/conf/server.xml
+sed -i 's/8009/8010/g' /data/apache-tomcat/conf/server.xml
+sed -i 's/8005/8015/g' /data/apache-tomcat/conf/server.xml
 rm /data/apache-tomcat-7.0.85.tar.gz
 echo -e "\eTomcat installation is done.."
