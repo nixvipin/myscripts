bb26324f681c52bd27a7a2aedde48f35a75367df
root <root@ip-172-31-19-255.us-west-2.compute.internal>
2018-04-21 05:04:53 +0000

    initial-commit

--- install_mysql.sh
+++ install_mysql.sh
@@ -20,4 +20,30 @@ mysqld --print-defaults
 read -p "Enter MySQL root password to login -->" password
 mysql -u root -p$password
 
-echo -e "\eYou're all set!"
+echo -e "\n Installation is done!\n"
+
+echo -e "\nSet the MySQL privileges using below commands\n"
+
+echo -e "mysql -uroot -p$password"
+
+echo -e "\ncreate database mydb;\n"
+
+echo "grant all on mydb.* to myuser@'localhost' identified by 'mypass';\n"
+
+echo "mysql -umyuser -pmypass mydb
+
+CREATE TABLE EMPLOYEE(
+    id INT NOT NULL auto_increment, 
+    name VARCHAR(50) NOT NULL,
+    joining_date DATE NOT NULL,
+    salary DOUBLE NOT NULL,
+    ssn VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+"
+
+echo -e "\nexit\n"
+
+echo "Done\n"
+
