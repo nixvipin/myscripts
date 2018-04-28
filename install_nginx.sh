fa1a549051c634e389b4a0522bb2e28ac5a14aac
root <root@ip-172-31-30-44.us-west-2.compute.internal>
2018-04-28 05:51:52 +0000

    initial-commit

--- install_nginx.sh
+++ install_nginx.sh
@@ -6,7 +6,7 @@ if [ $? != 0 ]
 then
 echo "ok.."
 else
-echo -e "\nChange below config in '/etc/selinux/config' file and try again.
+echo -e "\nDisable SELinux - Change below config in '/etc/selinux/config' file and restart VM or execute 'setenforce 0'.
 
 SELINUX=enforcing
 
@@ -21,7 +21,7 @@ fi
 
 yum install epel-release -y
 yum install nginx -y
-sed -s 's/80/8004/g' /etc/nginx/nginx.conf
+sed -i 's/80/8004/g' /etc/nginx/nginx.conf
 systemctl start nginx
 systemctl enable nginx
 systemctl status nginx
