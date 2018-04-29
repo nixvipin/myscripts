97e10a25cb65c37cc77e9ce3ce46650dca74ef9c
root <root@ip-172-31-30-44.us-west-2.compute.internal>
2018-04-29 10:52:06 +0000

    initial-commit

--- install_haproxy.sh
+++ install_haproxy.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
 yum install haproxy -y
-echo "Open /etc/haproxy/haproxy.cfg and add backend servers"
+
+echo -e "\n\e[32mEdit below configuration in /etc/haproxy/haproxy.cfg\e[0m
+
+
+#---------------------------------------------------------------------
+# main frontend which proxys to the backends
+#---------------------------------------------------------------------
+frontend  main *:80
+    mode                http
+    acl url_static       path_beg       -i /static /images /javascript /stylesheets
+    acl url_static       path_end       -i .jpg .gif .js
+
+    use_backend static          if url_static
+    default_backend             myproject
+
+	
+Add below configuration in /etc/haproxy/haproxy.cfg
+
+
+backend myproject
+    balance roundrobin
+    server web1 127.0.0.1:8001 check
+    server web1 127.0.0.1:8002 check
+
+\n\e[32mRestart HAProxy service 'systemctl restart haproxy'\e[0m\n"
+
