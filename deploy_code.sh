ad8c84721554c9c416c5a16979670a2b32624817
Vipin Khemani <nix.vipin@gmail.com>
2018-05-01 10:27:08 +0000

    initial-commit

--- deploy_code.sh
+++ deploy_code.sh
@@ -21,5 +21,25 @@ cd /data/apache-tomcat/bin
 ./startup.sh
 sleep 10
 tail -n 100 ../logs/catalina.out
-echo -e "\e[42m...Deployment is successfull..\e[0m"
+echo -e "\e[42m...Deployment is successfull on Tomcat1..\e[0m"
+
+if [ -d /data/apache-tomcat2 ]
+then
+cd /data/apache-tomcat2/bin
+./shutdown.sh
+sleep 10
+cd /data/apache-tomcat2/webapps
+rm -rf /data/apache-tomcat2/webapps/employee/*
+mkdir -p /data/apache-tomcat2/webapps/employee/
+cd /data/apache-tomcat2/webapps/employee/
+scp -i $PAM_FULL_PATH centos@$CLIENT_PRIVATE_IP:/data/backup/$SOURCE_BUILD_NUMBER/SpringHibernateExample.war .
+jar -xf SpringHibernateExample.war
+rm SpringHibernateExample.war
+cd /data/apache-tomcat2/bin
+./startup.sh
+sleep 10
+tail -n 100 ../logs/catalina.out
+echo -e "\e[42m...Deployment is successfull on Tomcat2..\e[0m"
+else
+echo "\nDone.\n"
 
