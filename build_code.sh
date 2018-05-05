235a7d66f55d5fcf1b4a9703f7c9d35b596e1b8f
Vipin Khemani <nix.vipin@gmail.com>
2018-05-05 08:46:48 -0400

    initial-commit

--- build_code.sh
+++ build_code.sh
@@ -7,17 +7,14 @@ CATALINA_BASE=/data/apache-tomcat
 
 export PATH JAVA_HOME MAVEN_HOME CATALINA_BASE BUILD_NUMBER
 
-cd /data/
-rm -rf /data/myproject/
-git clone https://github.com/nixvipin/myproject.git
-cd /data/myproject/employees
+cd /data/workspace/$JOB_NAME/myproject_local/employee
 mvn clean install
 
 if [ $? = 0 ]
 then
 echo -e "\n*** Build Succesfull ***\n"
 mkdir -p /data/backup/$BUILD_NUMBER
-cp -a /data/myproject/employees/target/SpringHibernateExample.war /data/backup/$BUILD_NUMBER/
+cp -a /data/workspace/$JOB_NAME/myproject_local/employee/target/SpringHibernateExample.war /data/backup/$BUILD_NUMBER/
 chown -R centos:centos /data/backup/$BUILD_NUMBER/
 else
 echo -e "\n*** Build is Failed ***\n"
