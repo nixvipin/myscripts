8ffc15a19cb69359d1e6476ab7963bda94317ad5
Vipin Khemani <nix.vipin@gmail.com>
2018-05-24 14:56:50 +0000

    initial-commit

--- install_nagios_server_npre.sh
+++ install_nagios_server_npre.sh
@@ -1,16 +1,18 @@
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install nagios-plugins-nrpe
 
-echo "\nEdit the Nagios configuration file to include all “*.cfg” files inside the “/usr/local/nagios/etc/servers” directory.
+echo -e "\n\e[32mSet Nagios GUI console password.\e[0m\n"
+
+mkdir /usr/local/nagios/etc/servers
+
+echo "\n1.\e[32mEdit the Nagios configuration file to include all “*.cfg” files inside the “/usr/local/nagios/etc/servers” directory.
 
 Add or uncomment the following line 'vi /usr/local/nagios/etc/nagios.cfg'
 
 cfg_dir=/usr/local/nagios/etc/servers
-"
 
-mkdir /usr/local/nagios/etc/servers
 
-echo "\nNow it’s time to configure the Nagios server to monitor the remote client machine, and You’ll need to create a command definition in Nagios object configuration file to use the “check_nrpe” plugin. Open the “commands.cfg” file…. 'vi /usr/local/nagios/etc/objects/commands.cfg'
+2. Now it’s time to configure the Nagios server to monitor the remote client machine, and You’ll need to create a command definition in Nagios object configuration file to use the “check_nrpe” plugin. Open the “commands.cfg” file…. 'vi /usr/local/nagios/etc/objects/commands.cfg'
 
 Add the following Nagios command definition to the file.
 
@@ -20,9 +22,9 @@ command_name check_nrpe
 command_line /usr/lib64/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -t 30 -c $ARG1$
 }
 
-"
 
-echo "\nNow create a client configuration file (/usr/local/nagios/etc/servers/server01.cfg) to define the host and service definitions of remote Linux host.
+
+3. Now create a client configuration file (/usr/local/nagios/etc/servers/server01.cfg) to define the host and service definitions of remote Linux host.
 
 define host{
 
@@ -105,14 +107,12 @@ define service{
 
 }
 
-"
 
-echo -e "\nNow Verify Nagios for any errors.
+4. Now Verify Nagios for any errors.
 
 /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
 
+5. Now check the Nagios GUI console to view the new services we added just now.
+\e[0m\n
 "
-
 service nagios restart
-
-echo -e "\nGo and check the Nagios web interface to view the new services we added just now."
