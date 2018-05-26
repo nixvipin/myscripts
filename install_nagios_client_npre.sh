7ee001e28868e94f803913e681c373a477d39b4c
Vipin Khemani <nix.vipin@gmail.com>
2018-05-26 08:01:34 +0000

    initial-commit

--- install_nagios_client_npre.sh
+++ install_nagios_client_npre.sh
@@ -2,7 +2,7 @@
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y nrpe nagios-plugins-all
 
-read -p "Enter your server host private IP Address >" ser_pip
+read -p "Enter your server Nagios server private IP Address >" ser_pip
 
 echo -e "\n\e[32mModify the NRPE configuration file to accept the connection from the Nagios server, Edit the /etc/nagios/nrpe.cfg file.
 
