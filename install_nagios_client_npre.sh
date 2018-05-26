b2a42ff17296c638351f81f6cfcee8717d75b2b6
Vipin Khemani <nix.vipin@gmail.com>
2018-05-26 08:03:43 +0000

    initial-commit

--- install_nagios_client_npre.sh
+++ install_nagios_client_npre.sh
@@ -4,7 +4,7 @@ yum install -y nrpe nagios-plugins-all
 
 read -p "Enter your server Nagios server private IP Address >" ser_pip
 
-echo -e "\n\e[32mModify the NRPE configuration file to accept the connection from the Nagios server, Edit the /etc/nagios/nrpe.cfg file.
+echo -e "\n\e[32mModify the NRPE configuration file to accept the connection from the Nagios server, Edit the /etc/nagios/nrpe.cfg file (line 117)
 
 allowed_hosts=127.0.0.1,$ser_pip
 
