6df67c67709b134945c0675fc4946780d0536836
Vipin Khemani <nix.vipin@gmail.com>
2018-06-10 05:43:01 +0000

    initial-commit

--- install_filebeat.sh
+++ install_filebeat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 read -p "Please enter your Elasticsearch(client01) server private IP address > " ela_ser_ip 
 
 rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
@@ -12,13 +11,14 @@ enabled=1
 gpgkey=https://packages.elastic.co/GPG-KEY-elasticsearch
 gpgcheck=1" > /etc/yum.repos.d/filebeat.repo
 
+yum remove filebeat -y; rm -rf /etc/filebeat
 yum -y  install filebeat
 
 sed -i "22i - /data/apache-tomcat/logs/*.log" /etc/filebeat/filebeat.yml
-sed -i "24i - /data/apache-tomcat/logs/*.txt" /etc/filebeat/filebeat.yml
-sed -i '25d' /etc/filebeat/filebeat.yml
-sed -i "83ihosts: ["$ela_ser_ip:9200"]" /etc/filebeat/filebeat.yml
-sed -i '84d' /etc/filebeat/filebeat.yml
+sed -i "23i - /data/apache-tomcat/logs/*.txt" /etc/filebeat/filebeat.yml
+sed -i '24d' /etc/filebeat/filebeat.yml
+sed -i "83ihosts: [\"$ela_ser_ip:9200\"]" /etc/filebeat/filebeat.yml
+sed -i '/localhost/d' /etc/filebeat/filebeat.yml
 systemctl enable filebeat
 systemctl start filebeat
 
