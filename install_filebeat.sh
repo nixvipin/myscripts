07c719a9e05c4faf8585b8379b2572f5d969f7ae
Vipin Khemani <nix.vipin@gmail.com>
2018-06-10 05:17:47 +0000

    initial-commit

--- install_filebeat.sh
+++ install_filebeat.sh
@@ -14,23 +14,12 @@ gpgcheck=1" > /etc/yum.repos.d/filebeat.repo
 
 yum -y  install filebeat
 
-<<<<<<< HEAD
-#sed -i "22i - /data/apache-tomcat/logs/*.log" /etc/filebeat/filebeat.yml
-#sed -i '23d' /etc/filebeat/filebeat.yml
-#sed -i "83ihosts: ["$ela_ser_ip:9200"]" /etc/filebeat/filebeat.yml
-=======
 sed -i "22i - /data/apache-tomcat/logs/*.log" /etc/filebeat/filebeat.yml
 sed -i "24i - /data/apache-tomcat/logs/*.txt" /etc/filebeat/filebeat.yml
 sed -i '25d' /etc/filebeat/filebeat.yml
 sed -i "83ihosts: ["$ela_ser_ip:9200"]" /etc/filebeat/filebeat.yml
 sed -i '84d' /etc/filebeat/filebeat.yml
->>>>>>> 28d8219a1a5a532de4233dc80e64f9f17db0a7c9
 systemctl enable filebeat
+systemctl start filebeat
 
-echo -e "\n\e[32mFollow steps below
-
-1. Copy contents of /etc/pki/tls/certs/logstash-forwarder.crt from client01 file and paste into /etc/pki/tls/certs/logstash-forwarder.crt on this machine(server01)
-
-2. Execute 'systemctl start filebeat' on server01
-
-\e[0m\n"
+echo -e "\n\e[32m..Done..\e[0m\n"
