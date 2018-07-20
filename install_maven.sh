e82021526e1a228de900dfc4574bd15be744c7ae
Vipin Khemani <nix.vipin@gmail.com>
2018-07-20 02:04:45 -0400

    initial-commit

--- install_maven.sh
+++ install_maven.sh
@@ -5,3 +5,13 @@ wget http://redrockdigimark.com/apachemirror/maven/maven-3/3.5.3/binaries/apache
 tar -xzf apache-maven-3.5.3-bin.tar.gz
 mv apache-maven-3.5.3 maven
 rm apache-maven-3.5.3-bin.tar.gz
+
+echo -e "\n\e[32mAdd below 3 lines in /etc/profile file
+
+MAVEN_HOME=/data/maven
+PATH=\$MAVEN_HOME/bin:\$PATH
+export PATH MAVEN_HOME\n
+
+And Execute 'source /etc/profile && mvn -version'\e[0m\e"
+
+
