934535bef870f38367a288a24fa8e7fd4fdcd994
Vipin Khemani <nix.vipin@gmail.com>
2018-07-20 02:07:00 -0400

    initial-commit

--- install_jdk.sh
+++ install_jdk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /data/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2F%www.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
-
 rpm -ivh jdk-8u131-linux-x64.rpm
+rm jdk-8u131-linux-x64.rpm
