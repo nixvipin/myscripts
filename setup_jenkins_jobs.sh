1febcdae1102c2c4cda267722691e7637f89b4c2
Vipin Khemani <nix.vipin@gmail.com>
2018-07-22 03:09:45 -0400

    initial-commit

--- setup_jenkins_jobs.sh
+++ setup_jenkins_jobs.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
+read -p "You existing jobs will be wiped-out.. continue..?(y/n) " ans
+
+if [ $ans = y ]
+then
+cp -rfa ~/.jenkins/jobs ~/.jenkins/jobs_bak
+rm -rf  ~/.jenkins/jobs/*
+elif [ $ans = n ]
+then
+echo -e "\nSee you next time..\n"
+exit 1
+else
+echo "\nSupported inputs y/n\n"
+exit 1
+fi
+
 build_setup()
 {
-rm -rf ~/.jenkins/jobs/myproject_build
 unzip myproject_build.zip -d ~/.jenkins/jobs/myproject_build
 }
 
 deploy_Setup()
 {
-rm -rf ~/.jenkins/jobs/myproject_deploy
-unzip myproject_deply.zip -d ~/.jenkins/jobs/myproject_deploy
+unzip myproject_deploy.zip -d ~/.jenkins/jobs/myproject_deploy
 }
 
 restart_jenkins()
 {
 cd /data/jenkins/bin
 ./shutdown.sh
+sleep 10
 ./startup.sh
 }
 
-read -p "\1. Build Job setup\2.Deploy Job setup\3.Both Build and Deploy Job setup\4.Exit" INPUT
+echo -e "\n1.Build Job setup\n2.Deploy Job setup\n3.Both Build and Deploy Job setup\n4.Exit\n"
+read -p "SELECT: " INPUT
 
 case $INPUT in
 	1)
 		build_setup
 		restart_jenkins
+		echo -e "\n\e[32mJenkins job setup is complete - http://Client_PublicIP:8080/jenkins\e[0m\n"
 		;;
 	2)
 		deploy_Setup
 		restart_jenkins
-		break
+		echo -e "\n\e[32mJenkins job setup is complete - http://Client_PublicIP:8080/jenkins\e[0m\n"
 		;;
 	3)
 		build_setup		
 		deploy_Setup
 		restart_jenkins	
+		echo -e "\n\e[32mJenkins job setup is complete - http://Client_PublicIP:8080/jenkins\e[0m\n"
 		;;
 	4)	echo "See you next time!"
 		exit 1
