72b78e7ce2840f3477a454a1c9a2e6075856b3f8
nixvipin <38068826+nixvipin@users.noreply.github.com>
2018-07-28 12:22:26 +0530

    Update install_puppet_server.sh

--- install_puppet_server.sh
+++ install_puppet_server.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#
+#This script to be executed on client01
+
 yum -y install ntp
 ntpdate pool.ntp.org
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
@@ -12,12 +16,12 @@ systemctl enable puppetserver
 echo -e "\nPlease make below entries in /etc/hosts file. For Example
 
 \e[32m
-172.31.30.44 server01 server01.us-west-2.compute.internal
-172.31.31.22 puppet client01.us-west-2.compute.internal
+172.31.30.44 server01
+172.31.31.22 puppet
 \e[0m
 
 # Replace IPs with you Server and Client Private IP Address repectively
-# and exeute 'sh install_puppet_agent.sh' on client machine"
+# and exeute 'sh install_puppet_agent.sh' on server01"
 
 mkdir -p /etc/puppetlabs/code/modules/mymodule/{files,manifests}
-echo "This file was present on puppet master" > /etc/puppetlabs/code/modules/mymodule/files/data.txt
+echo "This file was present on puppet master(client01)" > /etc/puppetlabs/code/modules/mymodule/files/data.txt
