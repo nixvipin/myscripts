c680ef98159427ceb25f894c295ec28c61187cb0
nixvipin <38068826+nixvipin@users.noreply.github.com>
2018-07-28 12:26:23 +0530

    Update install_puppet_agent.sh

--- install_puppet_agent.sh
+++ install_puppet_agent.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
@@ -5,18 +6,21 @@ yum -y install puppet-agent
 echo -e "\nMake the client entry in /etc/hosts file
 
 \e[32m
-172.31.30.44 server01 server01.us-west-2.compute.internal
-172.31.31.22 puppet client01.us-west-2.compute.internal
+172.31.30.44 server01
+172.31.31.22 puppet
 \e[0m
 
-#Replace IPs with you Client Machine Private IP Address\n"
+#Replace IPs with you Client and Server machines Private IP Address\n"
 
-echo -e "\nGo to the server and sign the certificate. For example
+echo -e "\e[32mYou will need sign the certificate.
 
-\e[32m
-/opt/puppetlabs/bin/puppet cert list
-/opt/puppetlabs/bin/puppet cert sign "server01.us-west-2.compute.internal"
-/opt/puppetlabs/bin/puppet cert list --all
+Execute below on server01 to send certificate request.
+1. puppet agent --test
+
+Execute below on client01 to sign the certificate
+puppet cert list
+puppet cert sign "server01.us-west-2.compute.internal"
+puppet cert list --all
 \e[0m"
 
 echo -e "\nThen create a file /etc/puppetlabs/code/environments/production/manifests/site.pp on puppet master and make below entry\n
@@ -30,7 +34,7 @@ file {'/tmp/example-ip':
 }
 
 
-node 'server01.us-west-2.compute.internal' {
+node 'server01' {
   class { 'apache': }
   apache::vhost { 'www.etlhive.com':
   port    => '80',
