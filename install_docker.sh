fc1a73faf885421a3d33a6bfd762df96000a8369
nixvipin <38068826+nixvipin@users.noreply.github.com>
2018-09-07 17:19:01 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -10,57 +10,55 @@ systemctl enable docker
 
 echo -e "\n\e[32mPlease follow below steps :
 
-1.  Log in on https://hub.docker.com/
-2.  Click on Create Repository.
-3.  Enter a project name and a description for your repository and click Create.
-4.  Execute on VM 'docker login' and enter your docker hub login details on your server
-5.  docker images
-6.  docker search centos
-7.  cd
-8.  mkdir mydocker
-9.  cd mydocker
-10. vim Dockerfile
+################################
+#   SIGNUP TO DOCKER REGISTRY  #
+################################
+
+1. Signup to https://hub.docker.com/
+2. Click on Create Repository.
+3. Enter a project name and a description for your repository and click Create.
+4. Execute on Docker Server 'docker login' and enter your docker hub login details on your server. You should see the message --> Login Succeeded.
+
+################################
+#   CREATE A CUSTOMIZED IMAGE  #
+################################
+
+1. docker images
+2. docker search centos
+4. mkdir /data/mydocker
+5. cd /data/mydocker
+6. vim Dockerfile
 
 FROM centos:latest
-MAINTAINER <Your_Mail_ID>
+MAINTAINER <Enter_Your_Mail_ID>
 RUN yum install httpd -y
 COPY index.html /var/www/html
 EXPOSE 80
 CMD [“/usr/sbin/httpd”, “-D”, “FOREGROUND”]
 
-11.  vim index.html
-
-<body style=\"background-color:powderblue;\">
+11. vim index.html
 
 <h1>This is my sample page</h1>
 <p>Hello! Docker</p>
 
-</body>
+12. docker build -t mydocker:1.0 .
+13. docker images
+14. docker run -d -p 8080:80 mydocker:1.0 /usr/sbin/httpd -D FOREGROUND
+15. docker ps
+16. docker exec -it <CONTAINER_ID> bash
+17. exit
+18. docker commit <CONTEINR_ID> mydocker:1.0
+19. docker tag mydocker:1.0 <DOCKER_USER_NAME>/mydockerimg
+20. docker images
+21. docker image push <DOCKER_USER_NAME>/mydockerimg
 
-12.  docker build -t mydocker:1.0 .
-13.  docker images
-14.  docker run -d -p 8080:80 mydocker:1.0 /usr/sbin/httpd -D FOREGROUND
-15.  docker ps
-16.  docker exec -it <CONTAINER_ID> bash
-17.  exit
-18.  docker commit <CONTEINR_ID> mydocker:1.0
-19.  docker tag mydocker:1.0 <DOCKER_USER_NAME>/mydockerimg
-20.  docker images
-21.  docker image push <DOCKER_USER_NAME>/mydockerimg
+##############################################
+#   DOWNLOAD NGINX IMAGE AND START CONTAINER #
+##############################################
 
-# Pull Ngnix image
-22.  docker pull nginx
-23.  docker run --name docker-nginx-new -p 8081:80 -e TERM=xterm -d nginx
-24.  docker exec -it <CONTAINER_ID> bash
-25.  You should be able to see Nginx default home page on when you hit http://192.168.56.102:8081 in browser.
+22. docker pull nginx
+23. docker run --name docker-nginx-new -p 8081:80 -e TERM=xterm -d nginx
+24. docker exec -it <CONTAINER_ID> bash
+25.  You should be able to see Nginx default home page on when you hit http://192.168.56.101:8081 in browser.
 
--->  Now let's have one more example setup Jenkins on port 8082
-26.  docker search jenkins
-27.  docker pull jenkins
-28.  docker images
-29.  docker run -d -p 8082:8080 -p 50000:50000 jenkins
-30.  You should be able to see Jenkins default home page on when you hit http://192.168.56.102:8082 in browser. 
-31.  docker ps
-32.  docker exec -it  CONTAINER_ID  bash
-33.  exit
 \e[0m\e"
