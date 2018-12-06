0d99fb985490ad9ed6886f954fcea05b7eb1a1a5
nixvipin <38068826+nixvipin@users.noreply.github.com>
2018-12-06 07:47:14 +0530

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -6,34 +6,29 @@ yum install ansible -y
 
 echo -e "\e[32m
 
--> Make below entry in /etc/ansible/hosts on client machine(Ansible Tower)
+-> Login to client01 machine 
+
+-> Execute below commands
+
+# cd /data
+# mkdir ansible
+# cd ansible
+#vim hosts
 
 [servers]
-server01 ansible_ssh_host=172.31.30.44
+server01 ansible_ssh_host=<REPLACE_SERVER_PRIVATE_IP> ansible_ssh_user=centos ansible_ssh_pass=<REPLACE_CENTOSUSER_SERVER01_PASSWORD>
 
-Note: Replace IP Address with your server machine private IP
+#ansible servers -m ping -i hosts
+#ansible -m shell -a 'free -m' 'server01'
+#'ansible -m ping server01'
 
--> Execute 'useradd ansible' on server01.
+-> Now install Nginx on server01 using ansible
 
--> Execute 'visudo' on server machine and add 'ansible ALL=(ALL)       NOPASSWD:ALL' in last line.
-
--> Execute 'useradd ansible' on client machine.
-
--> Execute 'su - ansible' then 'ssh-keygen'  on client machine, press blank enter until you get shell prompt.
-
--> Type 'exit'
-
--> Execute 'passwd ansible' on both client and server and set password 'ansible'.
-
--> Execute  'ssh-copy-id ansible@172.31.30.44' on your client machine. Replace IP with your server private IP address. Enter password 'ansible' (step 8).
-
--> Execute  'ansible all -m ping' and 'ansible -m shell -a 'free -m' 'server01' and 'ansible -m ping server01' on your client machine.
-
--> Now create a file 'vim nginx_install.yml' and copy contents below
+#vim nginx_install.yml
 
 
 ---
-- hosts: server01
+- hosts: servers
   tasks:
     - name: Install YUM repo
       command: yum install epel-release
@@ -54,38 +49,26 @@ Note: Replace IP Address with your server machine private IP
       become_user: root
 
 
-Note: Execute 'yum install epel-release -y' on 'server01' if the above is failed.
+#ansible-playbook nginx_install.yml
+#curl http://<REPLACE_SERVER01_PRIVATE_IP>
 
--> And your first playbook 'ansible-playbook nginx_install.yml' on client machine.
+-> Create another yaml to create a file
 
--> Now go to server machine and see process is running or not 'ps -ef | grep nginx'.
-
--> If the process is running hit your IP address in brower and see nginx default page 'http://YourServerPublicIP'.
-
--> Create another yaml to start Nginx service in client machine\(Ansible Tower\) 'vim nginx_start.yml'. Jump to \(step 16\) if process Nginx is already running.
+#vim create_file.yml
 
 ---
-- hosts: server01
-  tasks:
-    - name: Start NGiNX
-      service: name=nginx state=started
-      name: nginx
-      state: started
-      become: yes
-      become_method: sudo
-      become_user: root
-
--> Now execute if the Nginx service is not running 'ansible-playbook nginx_start.yml'
-
--> Create another yaml to create a user, install package, create a file 'vim user_create.yml'.
-
----
-- hosts: server01
+- hosts: servers
   tasks:
       - name: Create file
         file:
             path: /tmp/etlhive.txt
             state: touch
+
+#ansible-playbook -i hosts create_file.yml -v
+
+-> Create another yaml to create a user and install package in once.
+
+#vim create_user.yml
 
 - hosts: server01
   tasks:
@@ -99,10 +82,12 @@ Note: Execute 'yum install epel-release -y' on 'server01' if the above is failed
             name: zlib
             state: latest
 
+#ansible-playbook -i hosts user_create.yml -vv
 
--> And execute 'ansible-playbook user_create.yml'. Use 'ansible-playbook -vvv user_create.yml' to execute in debug mode.
+-> Create a yaml to copy file.
 
--> Create a yaml to copy file 'vim file_copy.yml' and execute.
+#echo "Hello, this is a test page." > /tmp/index.html
+#vim copy_file.yml
 
 ---
 - hosts: server01
@@ -112,12 +97,15 @@ Note: Execute 'yum install epel-release -y' on 'server01' if the above is failed
         become_method: sudo
         become_user: root
         template:
-            src:  /data/myscripts/index_html_nginx
+            src:  /tmp/index.html
             dest: /usr/share/nginx/html/index.html
 
--> Now see nginx page 'http://YourServerPublicIP'.
+#ansible-playbook -i hosts copy_file.yml -vv
+#curl http://<REPLACE_SERVER01_PRIVATE_IP>
 
--> Create a yaml to edit file 'vim file_edit.yml' and execute.
+-> Create a yaml to edit file
+
+#vim file_edit.yml
 
 ---
 - hosts: server01
@@ -138,6 +126,7 @@ Note: Execute 'yum install epel-release -y' on 'server01' if the above is failed
         become_user: root
 
 
--> Now you should see variable changes on 'http://YourServerPublicIP'.
+#ansible-playbook -i file_edit.yml -vvv
+#curl http://<REPLACE_SERVER01_PRIVATE_IP>
 
 \e[0m\n"
