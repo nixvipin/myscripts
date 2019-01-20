ccf2aef203b57f46079d396668f682b897fbfb77
nixvipin <38068826+nixvipin@users.noreply.github.com>
2019-01-20 15:22:43 +0530

    Update install_jdk.sh

--- install_jdk.sh
+++ install_jdk.sh
@@ -6,8 +6,8 @@ VER=8u201
 URL=https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-$VER-linux-x64.tar.gz
 
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3a%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;" "$URL"
-tar -xzf jdk-$VER-linux-x64.tar.gz --directory=/usr/local/jdk8
-rm -f jdk-$VER-linux-x64.tar.gz
+tar -xzf /tmp/jdk-$VER-linux-x64.tar.gz --directory=/usr/local/jdk8
+#rm -f /tmp/jdk-$VER-linux-x64.tar.gz
 
 grep JAVA_HOME /etc/profile
 
